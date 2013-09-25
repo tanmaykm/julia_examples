a8c23a9247384a7180d0ab7d4e35670458040910
Tanmay Mohapatra <tanmaykm@gmail.com>
2013-09-25 13:43:57 +0530

    Update runindexer.jl

--- common_crawl_indexer/runindexer.jl
+++ common_crawl_indexer/runindexer.jl
@@ -1,15 +1,12 @@
-
 if length(ARGS) < 2
     println("Usage: julia runindexer.jl <clustername> <num_archives_to_index>")
     exit()
 end
 
-include("ccconsts.jl")
-include("ccutils.jl")
+require("ccindexer.jl")
 
 start_ec2_cluster_workers()
 
 cc_use_local_node ? addprocs(cc_instnumworkers) : nothing
 
-require("ccindexer.jl")
 @time create_index(int(ARGS[2]))
