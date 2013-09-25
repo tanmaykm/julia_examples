84272cdce8b927530e04eb5319f3fbacdc07bb21
Amit Murthy <amit.murthy@gmail.com>
2013-09-25 13:30:42 +0530

    Update shutdown.jl

--- common_crawl_indexer/shutdown.jl
+++ common_crawl_indexer/shutdown.jl
@@ -1,10 +1,11 @@
-include("ccconsts.jl")
-include("ccutils.jl")
 
 if length(ARGS) < 1
     println("Usage: julia shutdown.jl <clustername>")
     exit()
 end
+
+include("ccconsts.jl")
+include("ccutils.jl")
 
 myinstances=AWS.EC2.ec2_instances_by_owner(get_cluster_name())
 AWS.EC2.ec2_terminate(myinstances)
