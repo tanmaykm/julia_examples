6f4c2122334a17c1f01b6d7c2be1588ea1fd8686
Tanmay Mohapatra <tanmaykm@gmail.com>
2013-09-25 13:43:27 +0530

    Update runsearcher.jl

--- common_crawl_indexer/runsearcher.jl
+++ common_crawl_indexer/runsearcher.jl
@@ -1,16 +1,12 @@
-
 if length(ARGS) < 2
     println("Usage: julia runsearcher.jl <clustername> \"search terms as a string\"")
     exit()
 end
 
-include("ccconsts.jl")
-include("ccutils.jl")
-
+require("ccsearcher.jl")
 
 start_ec2_cluster_workers()
 
 cc_use_local_node ? addprocs(cc_instnumworkers) : nothing
 
-require("ccsearcher.jl")
 @time search_index(ARGS[2])
